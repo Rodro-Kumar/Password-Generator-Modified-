a6b5e4000cc41a67437c6bc39d2b73ff4c9a5562
Rodro-Kumar <rudrokormokar96@gmail.com>
2023-12-30 12:37:20 +0600

    password generator

---
+++ Javascript/main.js
@@ -0,0 +1,54 @@
+const output = document.querySelector("#output");
+const l4Btn = document.querySelector("#btn1");
+const l8Btn = document.querySelector("#btn2");
+const l10Btn = document.querySelector("#btn3");
+const copyBtn = document.querySelector("#copyBtn");
+
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = uppercase.toLocaleLowerCase();
+const number = "1234567890";
+const special = "!@#$%&*";
+
+l4Btn.addEventListener("click", function () {
+  const upper = uppercase[Math.floor(Math.random() * uppercase.length)];
+  const lower = lowercase[Math.floor(Math.random() * lowercase.length)];
+  const num = number[Math.floor(Math.random() * number.length)];
+  const specialCha = special[Math.floor(Math.random() * special.length)];
+  let password = upper + lower + specialCha + num;
+  output.value = password;
+});
+
+const allItem = uppercase + lowercase + number + special;
+const length8 = 8;
+
+l8Btn.addEventListener("click", function () {
+  const upper = uppercase[Math.floor(Math.random() * uppercase.length)];
+  const lower = lowercase[Math.floor(Math.random() * lowercase.length)];
+  const num = number[Math.floor(Math.random() * number.length)];
+  const specialCha = special[Math.floor(Math.random() * special.length)];
+  let password = upper + lower + specialCha + num;
+  while (password.length <= length8) {
+    password += allItem[Math.floor(Math.random() * allItem.length)];
+  }
+  output.value = password;
+});
+
+const length10 = 10;
+
+l10Btn.addEventListener("click", function () {
+  const upper = uppercase[Math.floor(Math.random() * uppercase.length)];
+  const lower = lowercase[Math.floor(Math.random() * lowercase.length)];
+  const num = number[Math.floor(Math.random() * number.length)];
+  const specialCha = special[Math.floor(Math.random() * special.length)];
+  let password = upper + lower + specialCha + num;
+  while (password.length <= length10) {
+    password += allItem[Math.floor(Math.random() * allItem.length)];
+  }
+  output.value = password;
+});
+
+copyBtn.addEventListener("click", function () {
+  navigator.clipboard.writeText(output.value);
+  output.value = "copied";
+  copyBtn.innerHTML = "Password copied";
+});
